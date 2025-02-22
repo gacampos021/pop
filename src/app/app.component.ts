63597d43dd2ffc08cdf506beb1377c67cd1aaf45
Gabriel <gacampos021@gmail.com>
2025-02-22 14:28:22 -0300

    desing pop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgStyle, RouterLink ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
